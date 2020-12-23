9bbfb2cfd606a470b540c7e5b476f25a13598105
JordanPessman <jpessman22@gmail.com>
2020-12-23 01:40:28 -0600

    Netflix push

---
+++ src/Row.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import axios from './axios'
+import './Row.css'
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        //When this this loads run this code, the row.
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    console.table(movies);
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row__posters">
+                
+                {movies.map(movie => (
+                    <img key={movie.id} className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}></img>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Row
